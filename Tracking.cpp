1cb97d717ad6a42cdcf9835f2a4c188e285fbd68
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2016-04-19 14:16:59 +0800

    Update overlayImage() function

--- Tracking.cpp
+++ Tracking.cpp
@@ -89,7 +89,7 @@ void CObjectTracking::ObjectTrackingProcessing(Mat &img_input, Mat &img_output, 
 	drawTrajectory(img_input, TrackingLine, object_list, trigROI);
 
 	// Tracking image output (merge 3-channel image and 4-channel trakcing lines)
-	overlayImage(img_input, TrackingLine, img_output, cv::Point(0, 0));
+	overlayImage(img_input, TrackingLine, img_output);
 
 	runFirst = false;
 }
@@ -1600,49 +1600,24 @@ bool CObjectTracking::testIntraObjectIntersection(vector<ObjTrackInfo> &object_l
 *  This code is applied to merge two images of different channel, only works if:
 - The background is in BGR colour space.
 - The foreground is in BGRA colour space. */
-void CObjectTracking::overlayImage(const cv::Mat &background, const cv::Mat &foreground, cv::Mat &output, cv::Point2i location)
+void CObjectTracking::overlayImage(const cv::Mat &background, const cv::Mat &foreground, cv::Mat &output)
 {
 	background.copyTo(output);
-
-
+	
 	// start at the row indicated by location, or at row 0 if location.y is negative.
-	for (int y = MAX(location.y, 0); y < background.rows; ++y)
+	for (int y = 0; y < background.rows; ++y)
 	{
-		int fY = y - location.y; // because of the translation
-
-		// we are done of we have processed all rows of the foreground image.
-		if (fY >= foreground.rows)
-			break;
-
-		// start at the column indicated by location, 
-
-		// or at column 0 if location.x is negative.
-		for (int x = MAX(location.x, 0); x < background.cols; ++x)
+		for (int x = 0; x < background.cols; ++x)
 		{
-			int fX = x - location.x; // because of the translation.
+			// Determine the opacity of the foregrond pixel, using its fourth (alpha) channel.
+			double opacity = ((double)foreground.data[y * foreground.step + x * 4 + 3]) / 255.;
 
-			// we are done with this row if the column is outside of the foreground image.
-			if (fX >= foreground.cols)
-				break;
-
-			// determine the opacity of the foregrond pixel, using its fourth (alpha) channel.
-			double opacity =
-				((double)foreground.data[fY * foreground.step + fX * foreground.channels() + 3])
-
-				/ 255.;
-
-
-			// and now combine the background and foreground pixel, using the opacity, 
-
-			// but only if opacity > 0.
-			for (int c = 0; opacity > 0 && c < output.channels(); ++c)
+			// Only if opacity > 0.
+			for (int c = 0; opacity > 0 && c < 3; ++c)
 			{
-				unsigned char foregroundPx =
-					foreground.data[fY * foreground.step + fX * foreground.channels() + c];
-				unsigned char backgroundPx =
-					background.data[y * background.step + x * background.channels() + c];
-				output.data[y*output.step + output.channels()*x + c] =
-					(uchar)(backgroundPx * (1. - opacity) + foregroundPx * opacity);
+				unsigned char foregroundPx = foreground.data[y * foreground.step + x * 4 + c];
+				unsigned char backgroundPx = background.data[y * background.step + x * 3 + c];
+				output.data[y* output.step + 3 * x + c] = (uchar)(backgroundPx * (1. - opacity) + foregroundPx * opacity);
 			}
 		}
 	}
